a224c1f01eb0be71a49048264bbd842c0ba2c1ef
Jacob Grimshaw <j.grimshaw37@gmail.com>
2024-04-26 22:45:38 -0600

    removes unused variables

--- snake.py
+++ snake.py
@@ -4,11 +4,9 @@ import random
 pygame.init()
 
 white = (255, 255, 255)
-yellow = (255, 255, 102)
 black = (0, 0, 0)
 red = (213, 0, 0)
 green = (0, 255, 0)
-blue = (50, 153, 213)
 
 dis_width = 300
 dis_height = 200
